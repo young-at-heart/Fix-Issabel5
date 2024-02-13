87d0908bc6eed578b05cedac46a5513dd2086159
young-at-heart <37468538+young-at-heart@users.noreply.github.com>
2024-02-13 10:38:04 +0700

    Add files via upload

---
+++ fix-issabel5.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+### import modules
+import os
+
+print()
+a = input("Please enter asteriskuser password for mysql\n")
+b = input("Please enter root password for mysql\n")
+
+cmd = "grant all privileges on asteriskcdrdb.* to 'asteriskuser'@'localhost' identified by '"+a+"'"
+c = 'mysql -uroot -p'+b+' -e "'+cmd+'"'
+os.system(c)
+
+cmd = "use asteriskcdrdb; alter table cel alter eventextra set default 'NA'"
+c = 'mysql -uroot -p'+b+' -e "'+cmd+'"'
+os.system(c)
+
+os.system("cp -n queues.conf /etc/asterisk/")
+os.system("cp -n musiconhold.conf /etc/asterisk/")
+
+print("Fixing Issabel5 Completed!!!\n")
